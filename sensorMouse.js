67485b0abe591ccc36184f0ca091270b7a4eabd4
Amelia Růžek Klein <ARuzek@users.noreply.github.com>
2019-08-02 22:54:43 -0500

    Update sensorMouse.js

--- sensorMouse.js
+++ sensorMouse.js
@@ -13,7 +13,7 @@ navigator.bluetooth.requestDevice({
  ],
  // filters: [...] <- Prefer filters to save energy & show relevant devices.
  //acceptAllDevices: true
- optionalServices: ['00001801-0000-1000-8000-00805f9b34fb']
+ optionalServices: ['00001800-0000-1000-8000-00805f9b34fb']
 })
 .then(device => {
   console.log('Connecting to GATT Server...');
