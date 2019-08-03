2192ee8cdad16626ab58d7778e74c125688ade4d
Amelia Růžek Klein <ARuzek@users.noreply.github.com>
2019-08-03 10:23:03 -0500

    Update sensorMouse.js

--- sensorMouse.js
+++ sensorMouse.js
@@ -27,7 +27,7 @@ navigator.bluetooth.requestDevice({
   })
   .then(service => {
     console.log('Getting Characteristic...');
-    return service.getCharacteristic('00000001-000e-11e1-ac36-0002a5d5c51b');
+    return service.getCharacteristic('00000002-000e-11e1-ac36-0002a5d5c51b');
     //00000002-0001-11e1-ac36-0002a5d5c51b is Gesture Recognition
     //00000008-0001-11e1-ac36-0002a5d5c51b is Carry Position
     //00000010-0001-11e1-ac36-0002a5d5c51b is Activity Recognition
@@ -38,7 +38,7 @@ navigator.bluetooth.requestDevice({
     //04000000-0001-11e1-ac36-0002a5d5c51b is the Sound / Audio
     //08000000-0001-11e1-ac36-0002a5d5c51b is also Sound / Audio
     //40000000-0001-11e1-ac36-0002a5d5c51b is BV ADPCM Sync???
-    //00000002-000e-11e1-ac36-0002a5d5c51b is empty?
+    //00000001-000e-11e1-ac36-0002a5d5c51b is empty?
 
 
 
