232ade770ddd05e842f6e4e4749a5b9d1ff8aef7
Amelia Růžek Klein <ARuzek@users.noreply.github.com>
2019-08-02 19:32:28 -0500

    Update sensorMouse.js

--- sensorMouse.js
+++ sensorMouse.js
@@ -4,28 +4,28 @@ function runSensorMouse() {
 //  .split(/, ?/).map(s => s.startsWith('0x') ? parseInt(s) : s)
 //  .filter(s => s && BluetoothUUID.getService);
 
-log('Requesting any Bluetooth Device...');
+console.log('Requesting any Bluetooth Device...');
 navigator.bluetooth.requestDevice({
  // filters: [...] <- Prefer filters to save energy & show relevant devices.
     acceptAllDevices: true})
 .then(device => {
-  log('Connecting to GATT Server...');
+  console.log('Connecting to GATT Server...');
   return device.gatt.connect();
 })
 .then(server => {
   // Note that we could also get all services that match a specific UUID by
   // passing it to getPrimaryServices().
-  log('Getting Services...');
+  console.log('Getting Services...');
   return server.getPrimaryServices();
 })
 .then(services => {
-  log('Getting Characteristics...');
+  console.log('Getting Characteristics...');
   let queue = Promise.resolve();
   services.forEach(service => {
     queue = queue.then(_ => service.getCharacteristics().then(characteristics => {
-      log('> Service: ' + service.uuid);
+      console.log('> Service: ' + service.uuid);
       characteristics.forEach(characteristic => {
-        log('>> Characteristic: ' + characteristic.uuid + ' ' +
+        console.log('>> Characteristic: ' + characteristic.uuid + ' ' +
             getSupportedProperties(characteristic));
       });
     }));
@@ -33,7 +33,7 @@ navigator.bluetooth.requestDevice({
   return queue;
 })
 .catch(error => {
-  log('Argh! ' + error);
+  console.log('Argh! ' + error);
 });
 }
 
